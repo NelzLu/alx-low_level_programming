9f86637099e6d1a8145496b4c26af86a09303457
NelzLu <sanelesibeko96@gmail.com>
2023-02-16 13:57:34 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,5 +4,7 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{																						puts("\"Programming is like building a multilingual puzzle");													return: (0);
+{
+	puts("\"Programming is like building a multiligual puzzle");
+return (0);
 }
