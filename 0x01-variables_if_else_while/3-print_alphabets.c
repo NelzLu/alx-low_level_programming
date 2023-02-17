ba7af306e58bc17f66fef9c105ccfc73e491e121
NelzLu <sanelesibeko96@gmail.com>
2023-02-17 14:34:49 -0800

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,10 +9,10 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		purchar (ch);
+		putchar (ch);
 
 	for (ch = 'A'; ch <= 'Z'; ch++)
-		purchar (ch);
+		putchar (ch);
 
 	putchar('\n');
 
