b166468405ac338f47780f23bfcef577a3bcdf6f
NelzLu <sanelesibeko96@gmail.com>
2023-03-02 11:02:00 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include "main.h"
 int main(void)
 {
 	int n;
@@ -10,19 +10,13 @@ int main(void)
 	p = &n;
 /*
  * write your line of code here...
- 
  * Remember:
- 
  * - you are not allowed to use a
- 
  * - you are not allowed to modify p
- 
  * - only one statement
- 
  * - you are not allowed to code anything alse than this line of code
  */
 	*(p + 5) = 98;
-	
 /*...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
