7dca5eb094d9d9bfffd357c649ee3e287d1435c5
NelzLu <sanelesibeko96@gmail.com>
2023-03-02 11:21:24 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,13 +12,11 @@ char *cap_string(char *str)
 	int index = 0;
 
 	while (str[index])
-
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-
 			index++;
 
-		if (str[index - 1] == ' ' |
+		if (str[index - 1] == ' ' ||
 		str[index - 1] == '\t' ||
 		str[index - 1] == '\n' ||
 		str[index - 1] == ',' ||
@@ -32,11 +30,8 @@ char *cap_string(char *str)
 		str[index - 1] == '{' ||
 		str[index - 1] == '}' ||
 		index == 0)
-
 			str[index] -= 32;
-
 		index++;
 	}
 	return (str);
-
 }
