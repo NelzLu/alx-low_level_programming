cf261c492a85d415a7e8cc396bfd05e745931516
NelzLu <sanelesibeko96@gmail.com>
2023-03-02 09:18:03 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,24 +9,26 @@
 
 char *cap_string(char *str)
 {
+
 	int index = 0;
 
 	while (str[index])
 
 	{
 
-		while (!(str[index] >= 'a' && str[index] <= 'z))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 
 			index++;
 
-		if (str[index - 1] == ' ' ||
+
+		if (str[index - 1] == ' ' |
 		str[index - 1] == '\t' ||
 		str[index - 1] == '\n' ||
-		str[index - 1] == ', '  ||
-		str[index - 1] == '; ' ||
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
 		str[index - 1] == '.' ||
-		str[index - 1] == ' !' ||
-		str[index - 1] == ' ? ' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
 		str[index - 1] == '"' ||
 		str[index - 1] == '(' ||
 		str[index - 1] == ')' ||
@@ -40,5 +42,7 @@ char *cap_string(char *str)
 
 	}
 
+
 	return (str);
+
 }
